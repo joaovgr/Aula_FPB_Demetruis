a9e89a9436ad48eb136eaedd8e6e06b40c0ea5d4
joaovgr <128102271+joaovgr@users.noreply.github.com>
2023-03-24 21:30:27 -0300

    Add files via upload

---
+++ JoãoVictor/joao.php
@@ -0,0 +1,25 @@
+<?php
+
+$nota1 = 7;
+$nota2 = 8;
+$nota3 = 9;
+
+$media = (($nota1 + $nota2 + $nota3)/ 3);
+if($media >= 7 ) {
+    echo"APROVADOOOOO!😍😍😍😍😍!";
+}
+
+elseif(($media < 7) && ($media >= 4)){
+    echo"Prova Final!😒😒";
+}
+
+else {
+    echo"Reprovado!🤣👌";
+}
+    
+
+
+
+
+
+?>
